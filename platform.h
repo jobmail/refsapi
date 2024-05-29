7d11fa1c5e33e742bad9f7532d912c44ec18c71e
Refresh <jobmail@yandex.ru>
2024-05-30 00:13:02 +0300

    fix

--- platform.h
+++ platform.h
@@ -2,19 +2,23 @@
 
 #undef DLLEXPORT
 #ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
-#define NOINLINE __declspec(noinline)
-#define snprintf _snprintf
+#define DLLEXPORT       __declspec(dllexport)
+#define NOINLINE        __declspec(noinline)
+#define snprintf        _snprintf
 #else
-#define DLLEXPORT __attribute__((visibility("default")))
-#define NOINLINE __attribute__((noinline))
-#define WINAPI		/* */
+#define DLLEXPORT       __attribute__((visibility("default")))
+#define NOINLINE        __attribute__((noinline))
+#define WINAPI		    /* */
+#define _stricmp        strcasecmp
+#define	_strnicmp       strncasecmp
+#define _vsnprintf      vsnprintf
+#define _snprintf       snprintf
 #endif
 
 #ifndef _MSC_VER
-#define likely(x)      __builtin_expect(!!(x), 1)
-#define unlikely(x)    __builtin_expect(!!(x), 0)
+#define likely(x)       __builtin_expect(!!(x), 1)
+#define unlikely(x)     __builtin_expect(!!(x), 0)
 #else
-#define likely(x)      (x)
-#define unlikely(x)    (x)
+#define likely(x)       (x)
+#define unlikely(x)     (x)
 #endif
