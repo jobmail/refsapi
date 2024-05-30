aa148afb0621dc5e2fc7adf2d6ef7d7d5a7a3cbd
Refresh <jobmail@yandex.ru>
2024-05-30 21:18:56 +0300

    fix

--- refsapi.cpp
+++ refsapi.cpp
@@ -7,9 +7,10 @@ void R_ClientPutInServer_Post(edict_t *pEntity) {
 	
     if (!pPlayer->IsBot()) {
     
-        SERVER_PRINT("[DEBUG] R_ClientPutInServer_Post() ===>");
-        //UTIL_ServerPrint("PutInserver_Post(): %s <%s>", pPlayer->pev->netname, GETPLAYERAUTHID(pPlayer->edict()));
-	
+        SERVER_PRINT("[DEBUG] R_ClientPutInServer_Post() ===>\n");
+
+        UTIL_ServerPrint("PutInserver_Post(): %s <%s>\n", STRING(pPlayer->pev->netname), GETPLAYERAUTHID(pPlayer->edict()));
+
     }
     SET_META_RESULT(MRES_IGNORED);
 }
