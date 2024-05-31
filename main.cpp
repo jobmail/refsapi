39903acfcc6b22fd4e7aa918f461c2188507c81b
Refresh <jobmail@yandex.ru>
2024-05-31 23:36:30 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ void ServerActivate_Post(edict_t *pEdictList, int edictCount, int clientMax)
 		msg.id = GET_USER_MSG_ID(PLID, msg.pszName, NULL);
 	}
 
-	memset(g_PlayersNum, 0, sizeof(g_PlayersNum));
+	//memset(g_PlayersNum, 0, sizeof(g_PlayersNum));
 
 	SET_META_RESULT(MRES_IGNORED);
 }
