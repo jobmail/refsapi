16acc77df03e44ca87ac6e7d5925f7bc90c209da
Refresh <jobmail@yandex.ru>
2024-05-31 14:07:12 +0300

    fix

--- refsapi.cpp
+++ refsapi.cpp
@@ -23,7 +23,7 @@ edict_t* R_ED_Alloc(IRehldsHook_ED_Alloc* chain) {
 
     int id = ENTINDEX(origin);
 
-    if (id > 0 || id <= gpGlobals->maxClients)
+    if (id > 0 && id <= gpGlobals->maxClients)
 
         UTIL_ServerPrint("[DEBUG] ED_Alloc(): id = %d", id);
 
