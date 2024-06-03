bf375aa65e549cee9addd80c2a0ba456c1b3a1f2
Refresh <jobmail@yandex.ru>
2024-06-03 06:28:47 +0300

    fix

--- refsapi.h
+++ refsapi.h
@@ -1,4 +1,7 @@
 #pragma once
+// gets rid of annoying "deprecated conversion from string constant blah blah" warning
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
 
 #include <map>
 #include <precompiled.h>
