c58bdd27ce6f6eab03393a51efb41ee516db17bd
Refresh <jobmail@yandex.ru>
2024-06-10 15:01:19 +0300

    fix

--- refsapi.cpp
+++ refsapi.cpp
@@ -274,6 +274,10 @@ void CBasePlayer_Killed_RG(IReGameHook_CBasePlayer_Killed *chain, CBasePlayer *p
     if (g_Clients[pPlayer->entindex()].is_connected && pPlayer->m_iTeam >= 1 && pPlayer->m_iTeam <=2)
 
         g_PlayersNum[TEAM_DEAD_TT + pPlayer->m_iTeam - 1]++;
+    
+    UTIL_ServerPrint("[DEBUG] num_unassigned = %d, num_tt = %d, num_ct = %d, num_spec = %d, num_dead_tt = %d, num_dead_ct = %d\n",
+        g_PlayersNum[TEAM_UNASSIGNED], g_PlayersNum[TEAM_TERRORIST], g_PlayersNum[TEAM_CT], g_PlayersNum[TEAM_SPECTRATOR], g_PlayersNum[TEAM_DEAD_TT], g_PlayersNum[TEAM_DEAD_CT]
+    );
 
     chain->callNext(pPlayer, pevAttacker, iGib);
 }
@@ -347,16 +351,20 @@ void Client_Disconnected(int id, bool crash, char *format) {
 
                 //UTIL_ServerPrint("[DEBUG] Client_Disconnected(): id = %d, team = %d, count = %d << WARNING !!!", id, g_Clients[id].team, g_PlayersNum[g_Clients[id].team]);
 
-            CBasePlayer *pPlayer = UTIL_PlayerByIndexSafe(id);
+            //CBasePlayer *pPlayer = UTIL_PlayerByIndexSafe(id);
+
+            edict_t* pEdict = INDEXENT(id);
 
             //UTIL_ServerPrint("[DEBUG] Client_Disconnected(): id = %d, name = %s\n", id, STRING(pPlayer->edict()->v.netname));
 
-            if (pPlayer != nullptr && pPlayer->edict()->v.deadflag != DEAD_NO && g_Clients[id].team >= TEAM_TERRORIST && g_Clients[id].team <= TEAM_CT) {
+            if (is_valid_entity(pEdict) && pEdict->v.deadflag != DEAD_NO && g_Clients[id].team >= TEAM_TERRORIST && g_Clients[id].team <= TEAM_CT) {
 
-                g_PlayersNum[TEAM_DEAD_TT + g_Clients[id].team - 1]++;
+                g_PlayersNum[TEAM_DEAD_TT + g_Clients[id].team - 1]--;
             }
 
-            //UTIL_ServerPrint("[DEBUG] num_unassigned = %d, num_tt = %d, num_ct = %d, num_spec = %d\n", g_PlayersNum[TEAM_UNASSIGNED], g_PlayersNum[TEAM_TERRORIST], g_PlayersNum[TEAM_CT], g_PlayersNum[TEAM_SPECTRATOR]);
+            UTIL_ServerPrint("[DEBUG] num_unassigned = %d, num_tt = %d, num_ct = %d, num_spec = %d, num_dead_tt = %d, num_dead_ct = %d\n",
+                g_PlayersNum[TEAM_UNASSIGNED], g_PlayersNum[TEAM_TERRORIST], g_PlayersNum[TEAM_CT], g_PlayersNum[TEAM_SPECTRATOR], g_PlayersNum[TEAM_DEAD_TT], g_PlayersNum[TEAM_DEAD_CT]
+            );
         }
     }
 }
