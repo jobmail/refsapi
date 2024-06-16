4cf43ce1e66a8119efd71e75721f037d04c9c213
Refresh <jobmail@yandex.ru>
2024-06-16 18:33:10 +0300

    fix

--- refsapi.cpp
+++ refsapi.cpp
@@ -721,7 +721,7 @@ wchar_t* wfmt(wchar_t *fmt, ...) {
 
     va_start(arg_ptr, fmt);
 
-    _vsnwprintf(g_wfmt_buff, sizeof(g_wfmt_buff) - 1, fmt, arg_ptr);
+    Q_vsnwprintf(g_wfmt_buff, sizeof(g_wfmt_buff) - 1, fmt, arg_ptr);
 
     va_end(arg_ptr);
 
