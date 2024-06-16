72bd69e46701b78947601a97195e4631d1f77c9c
Refresh <jobmail@yandex.ru>
2024-06-16 15:40:37 +0300

    fix

--- natives_refsapi.cpp
+++ natives_refsapi.cpp
@@ -189,7 +189,7 @@ cell AMX_NATIVE_CALL rf_config(AMX *amx, cell *params) {
 
         UTIL_ServerPrint("[DEBUG] rf_config(): exist = %d\n", is_exist);
 
-        std::fstream file(path);
+        std::fstream file(path, std::ios::in | std::ios::out);
     
         UTIL_ServerPrint("[DEBUG] rf_config(): is_open = %d\n", file.is_open());
 
