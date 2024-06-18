0935fcdd6164d2b75329ab2d07413c9d723286c3
Refresh <jobmail@yandex.ru>
2024-06-18 03:41:58 +0300

    fix

--- natives_refsapi.cpp
+++ natives_refsapi.cpp
@@ -229,19 +229,19 @@ cell AMX_NATIVE_CALL rf_config(AMX *amx, cell *params) {
 
                     // CHECK
 
-                    auto cvar_result = g_cvar_mngr.get(plugin, var_name);
+                    auto cvar_it = g_cvar_mngr.get(plugin, var_name);
 
-                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_result->second, cvar_result->first->second.cvar->name, cvar_result->first->second.cvar->string);
+                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_it->second, cvar_it->second.cvar->name, cvar_it->second.cvar->string);
 
                     g_cvar_mngr.set(plugin, var_name, L"123.45678900000123");
 
-                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_result->second, cvar_result->first->second.cvar->name, cvar_result->first->second.cvar->string);
+                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_it->second, cvar_it->second.cvar->name, cvar_it->second.cvar->string);
 
-                    //cvar_result = g_cvar_mngr.get(plugin, var_name);
+                    //cvar_it = g_cvar_mngr.get(plugin, var_name);
 
-                    g_cvar_mngr.set(plugin, cvar_result, L"987.654321000111222");
+                    g_cvar_mngr.set(plugin, cvar_it, L"987.654321000111222");
 
-                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_result->second, cvar_result->first->second.cvar->name, cvar_result->first->second.cvar->string);
+                    UTIL_ServerPrint("[DEBUG] rf_config(): CHECK ==> exist = %d, name = <%s>, value = <%s>\n", cvar_it->second, cvar_it->second.cvar->name, cvar_it->second.cvar->string);
                 }
 
             } else {
