e5525b806474febe36a3d14c0b7e3bdf8389f68c
Refresh <jobmail@yandex.ru>
2024-06-21 00:01:25 +0300

    fix

--- refsapi_cvar_mngr.h
+++ refsapi_cvar_mngr.h
@@ -111,12 +111,12 @@ public:
             cvars.bind[cvar_it] = bind_list;
     }
     */
-    void on_change(cvar_list_it cvar_it, std::string &new_value)
+    void on_change(cvar_list_it cvar, std::string &new_value)
     {
         //UTIL_ServerPrint("[DEBUG] on_change(): name = %s, old_value = %s, new_value = %s\n", wstos(cvar_it->second.name).c_str(), wstos(cvar_it->second.value).c_str(), new_value.c_str());
         cvar_bind_it bind_it;
         // Bind exists?
-        if ((bind_it = cvars.bind.find(cvar_it)) != cvars.bind.end())
+        if ((bind_it = cvars.bind.find(cvar)) != cvars.bind.end())
         {
             //auto cvar_bind = bind_it->second;
                 /*
