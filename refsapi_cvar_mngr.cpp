ddc8e96d2e09fe7b5ef88c1d8b1d346f518e17a2
Refresh <jobmail@yandex.ru>
2024-06-21 19:57:37 +0300

    fix

--- refsapi_cvar_mngr.cpp
+++ refsapi_cvar_mngr.cpp
@@ -4,7 +4,6 @@ void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const ch
 {
     chain->callNext(cvar, value);
     cvar_list_it cvar_list = g_cvar_mngr.get(cvar);
-    UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_RH(): old_value = %s, old_cvar_value = %s, new_value = %s\n", wstos(cvar_list->second.value).c_str(), cvar->string, value);
     // Cvar not register?
     if (check_it_empty(cvar_list))
     {
@@ -16,13 +15,13 @@ void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const ch
     }
     else
     {
-        return;
+        UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_RH(): old_value = %s, old_cvar_value = %s, new_value = %s\n", wstos(cvar_list->second.value).c_str(), cvar->string, value);
         m_cvar_t* m_cvar = &cvar_list->second;
         // Convert to string
         std::string s = value;
         // Is callback after fix value or samething went wrong?
-        //if (m_cvar->value.compare(stows(s)) == 0)//(m_cvar->value.size() == s.size() && std::equal( begin(), m_cvar->value.end(), ))
-        //    return;
+        if (m_cvar->value.compare(stows(s)) == 0)//(m_cvar->value.size() == s.size() && std::equal( begin(), m_cvar->value.end(), ))
+            return;
         // Is number?
         if (is_number(s))
             s = rtrim_zero_c(std::to_string(stod(s, m_cvar)));
