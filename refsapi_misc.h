a8171d8840409ccaf3b5fa6cf2f07a131736dee6
Refresh <jobmail@yandex.ru>
2024-06-21 10:10:43 +0300

    fix

--- refsapi_misc.h
+++ refsapi_misc.h
@@ -147,7 +147,7 @@ inline std::wstring stows(const std::string &s)
 
 double zzz(std::string s, bool has_min = false, float min_val = 0.0f, bool has_max = false, float max_val = 0.0f)
 {
-    auto result = std::stod(s.c_str());
+    auto result = std::stod(s);
     UTIL_ServerPrint("[DEBUG] stof(): in = %s, out = %Lf\n", s.c_str(), result);
     if (has_min && result < min_val)
         result = min_val;
