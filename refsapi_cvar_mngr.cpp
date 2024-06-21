2dda831270e852c813b369fd467bb3b349cf0fd8
Refresh <jobmail@yandex.ru>
2024-06-21 19:43:14 +0300

    fix

--- refsapi_cvar_mngr.cpp
+++ refsapi_cvar_mngr.cpp
@@ -20,8 +20,8 @@ void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const ch
         // Convert to string
         std::string s = value;
         // Is callback after fix value or samething went wrong?
-        if (m_cvar->value.compare(stows(s)) == 0)//(m_cvar->value.size() == s.size() && std::equal( begin(), m_cvar->value.end(), ))
-            return;
+        //if (m_cvar->value.compare(stows(s)) == 0)//(m_cvar->value.size() == s.size() && std::equal( begin(), m_cvar->value.end(), ))
+        //    return;
         // Is number?
         if (is_number(s))
             s = rtrim_zero_c(std::to_string(stod(s, m_cvar)));
