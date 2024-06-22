67910a31f6c261dfb5087689af247599092bc04e
Refresh <jobmail@yandex.ru>
2024-06-22 23:49:27 +0300

    fix

--- refsapi_cvar_mngr.cpp
+++ refsapi_cvar_mngr.cpp
@@ -9,7 +9,7 @@ void Cvar_DirectSet_Post(cvar_t *cvar, const char *value)
 void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const char *value)
 {
     chain->callNext(cvar, value);
-    UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_RH(): cvar = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
+    //UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_RH(): cvar = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
     g_cvar_mngr.on_direct_set(cvar, value);
 }
 
