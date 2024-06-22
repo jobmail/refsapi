bbda280df0f71aea5a919585d2396b15bcd4d87b
Refresh <jobmail@yandex.ru>
2024-06-22 23:28:09 +0300

    fix

--- refsapi_cvar_mngr.cpp
+++ refsapi_cvar_mngr.cpp
@@ -1,10 +1,14 @@
 #include "precompiled.h"
 
 //void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const char *value)
-//chain->callNext(cvar, value);
 void Cvar_DirectSet_Post(cvar_t *cvar, const char *value)
 {
-    UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_Post(): cvar = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
+    //UTIL_ServerPrint("[DEBUG] Cvar_DirectSet_Post(): cvar = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
+}
+
+void Cvar_DirectSet_RH(IRehldsHook_Cvar_DirectSet *chain, cvar_t *cvar, const char *value)
+{
+    chain->callNext(cvar, value);
     g_cvar_mngr.on_direct_set(cvar, value);
 }
 
