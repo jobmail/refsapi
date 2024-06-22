b4ab13b4342838f01e51142683be5a60afd3a491
Refresh <jobmail@yandex.ru>
2024-06-22 21:18:15 +0300

    fix

--- refsapi_cvar_mngr.h
+++ refsapi_cvar_mngr.h
@@ -194,7 +194,7 @@ public:
         // Bind none-exists?
         if (m_cvar->type == CVAR_TYPE_NONE)
         {
-            UTIL_ServerPrint("[DEBUG] on_direct_set(): NOT BIND => name = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
+            //UTIL_ServerPrint("[DEBUG] on_direct_set(): NOT BIND => name = <%s>, string = <%s>, value = %f\n", cvar->name, cvar->string, cvar->value);
             m_cvar->value = stows(value);
             return;
         }
