42f894d973049905c4fa7db08c311cacb3e96375
Refresh <jobmail@yandex.ru>
2024-06-23 21:07:02 +0300

    fix

--- h_export.cpp
+++ h_export.cpp
@@ -7,11 +7,21 @@ globalvars_t  *gpGlobals;
 
 enginefuncs_t* meta_hack;
 void (*meta_Cvar_RegisterVariable)(cvar_t *pCvar) = nullptr;
+void (*meta_CvarRegister)(cvar_t *pCvar) = nullptr;
+
 void Cvar_RegisterVariable(cvar_t *pCvar)
 {
-	UTIL_ServerPrint("\n[DEBUG] [***  Cvar_RegisterVariable ***]: meta hook !!!!!!!!\n\n");
+	UTIL_ServerPrint("\n[DEBUG] [***  Cvar_RegisterVariable ***]: meta hook #1 !!!!!!!!\n\n");
 	if (meta_Cvar_RegisterVariable != nullptr)
 		meta_Cvar_RegisterVariable(pCvar);
+	Cvar_RegisterVariable_Post(pCvar);
+}
+
+void CvarRegister(cvar_t *pCvar)
+{
+	UTIL_ServerPrint("\n[DEBUG] [###  Cvar_Register ###]: meta hook #2!!!!!!!!\n\n");
+	if (meta_CvarRegister != nullptr)
+		meta_CvarRegister(pCvar);
 	Cvar_RegisterVariable_Post(pCvar);
 }
 
@@ -30,6 +40,8 @@ C_DLLEXPORT void WINAPI GiveFnptrsToDll(enginefuncs_t *pengfuncsFromEngine, glob
 	meta_hack = pengfuncsFromEngine; //(enginefuncs_t*)gpMetaGlobals->orig_ret;
 	UTIL_ServerPrint("\n[DEBUG] [Cvar_RegisterVariable]: meta hook %d!!!!!!!!\n", meta_hack);
 	meta_Cvar_RegisterVariable = meta_hack->pfnCvar_RegisterVariable; // pfnCvar_RegisterVariable; //g_engine.pl_funcs.pfnCVarRegister;
+	meta_CvarRegister = meta_hack->pfnCVarRegister;
 	meta_hack->pfnCvar_RegisterVariable = Cvar_RegisterVariable; //g_engine.pl_funcs.pfnCVarRegister
+	meta_hack->pfnCVarRegister = CvarRegister;
 	//meta_hack->pfnCVarRegister = Cvar_RegisterVariable;
 }
