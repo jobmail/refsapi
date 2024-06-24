cb716fb7c68c77b9bb505dc739227a9d032ff671
Refresh <jobmail@yandex.ru>
2024-06-24 20:56:07 +0300

    fix

--- refsapi_cvar_mngr.h
+++ refsapi_cvar_mngr.h
@@ -367,7 +367,7 @@ public:
                     result = (int)m_cvar->cvar->value; //std::stof(cvar->string);
                     break;
                 case CVAR_TYPE_FLT:
-                    result = (float)std::stod(m_cvar->cvar->string); //amx_ftoc(((float)std::stod(m_cvar->cvar->string))); //m_cvar->cvar->value
+                    result = amx_ftoc((float)std::stod(m_cvar->cvar->string)); //amx_ftoc(((float)std::stod(m_cvar->cvar->string))); //m_cvar->cvar->value
                     break;
                 case CVAR_TYPE_STR:
                     std::string s = wstos(m_cvar->value);
